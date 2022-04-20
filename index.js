e6ce3a4bf27e1a9056fab9a4586f91bd25db7373
Max-Barron <max@enterpriseautomation.co.uk>
2022-04-20 16:04:35 +0100

    update

--- index.js
+++ index.js
@@ -43,7 +43,34 @@ router.get('/:id', function (req, res, next) {
     }, function (err) {
         next(err);
     });
-})
+});
+
+router.get('/:200', function (req, res, next) {
+    quoteRepo.getRandom(function (data) {
+        if (data) {
+            res.status(200).json({
+                "status": 200,
+                "statusText": "ok",
+                "message": "Random quote retrieved",
+                "data": data
+            });
+        }
+        else {
+            res.status(404).json({
+                "status": 404,
+                "statusText": "Not Found",
+                "message": "The quote '" + req.params.id + "' could not be found.",
+                "error": {
+                    "code": "NOT_FOUND",
+                    "message": "The quote '" + req.params.id + "' could not be found."
+                }
+            });
+        }
+    }, function (err) {
+        next(err);
+    });
+});
+
 
 app.use('/api/', router);
 var server = app.listen(3000, function () {
--- repo/quoteRepo.js
+++ repo/quoteRepo.js
@@ -1,28 +1,68 @@
 const fs = require('fs');
-const QUOTES_FILE = './utils/quotes.json'
+const mysql = require("mysql")
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Renegade187!',
+    database: 'mysql'
+});
+
+connection.connect();
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+const rndInt2 = randomIntFromInterval(1, 24)
 
 let quoteRepo = {
     get: function (resolve, reject) {
-        fs.readFile(QUOTES_FILE, function (err, data) {
+        connection.query("SELECT * FROM `quotes`", function (err, result, fields) {
             if (err) {
                 reject(err);
             }
             else {
-                resolve(JSON.parse(data));
+                resolve(result);
             }
         });
     },
     getById: function (id, resolve, reject) {
-        fs.readFile(QUOTES_FILE, function (err, data) {
+        connection.query(`SELECT * FROM quotes WHERE id="${id}"`, function (err, result, fields) {
             if (err) {
-                reject(err);
+                reject(err)
+            } else {
+                resolve(result);
             }
-            else {
-                let pie = JSON.parse(data).find(p => p.id == id);
-                resolve(pie);
+
+        });
+    },
+    getRandom: function (random, resolve, reject) {
+        connection.query(`SELECT * FROM quotes WHERE id= ${rndInt2}`, function (err, result, fields) {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(result);
             }
         });
+    },
+    rndInt2: function (min, max) { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min)
     }
+
 };
 
 module.exports = quoteRepo;
+
+
+    //     getById: function (id, resolve, reject) {
+    //         fs.readFile(QUOTES_FILE, function (err, data) {
+    //             if (err) {
+    //                 reject(err);
+    //             }
+    //             else {
+    //                 let pie = JSON.parse(data).find(p => p.id == id);
+    //                 resolve(pie);
+    //             }
+    //         });
+    //     }
+    // };
